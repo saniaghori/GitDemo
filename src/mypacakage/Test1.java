fbfe0ae3b59ad51cd7fd53d133fbbf548de1d208
sania <ghorisania03@gmail.com>
2024-03-26 16:27:28 -0700

    first commit

---
+++ src/mypacakage/Test1.java
@@ -0,0 +1,18 @@
+package mypacakage;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+	@Test
+	public void test() {
+		System.out.println("hi");
+		
+	
+	}
+	@Test
+	public void data() {
+		System.out.println("Hello world");
+	}
+	}
+
+
